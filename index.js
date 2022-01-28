8a5d8a834876422a64afdeecb7f59fca53156bb7
Julian <julian_delisio@hotmail.com>
2022-01-28 01:00:55 -0300

    Se Agrega Post y put

--- index.js
+++ index.js
@@ -1,17 +1,62 @@
 const express = require("express");
 
-const data = require("./MOCK_DATA.json")
+const Service = require("./src/service");
 
 const app = express();
 
 const PORT = 3000;
 
+app.use(express.json()); // Permite recibir datos Json
+
 app.get("/", (req, res) => {
     res.json({
         message: "Lista de usuarios",
-        body: data
+        body: Service.getUsers()
     });
 });
+
+app.get("/:id", (req, res) => { // Obtenemos el ID por parametro
+    let id = req.params.id;
+    let user = Service.getUser(id);
+    res.json({
+        message: `Usuario ${id}`,
+        body: user
+    });
+});
+
+app.post("/", (req, res) => {
+    let newUser = req.body;
+    Service.createUser(newUser);
+    res.status(201).json({
+        message: "Usuario creado",
+        body: newUser
+    })
+})
+
+app.put("/:id", (req, res) => {
+    const user = Service.getUser(req.params.id);
+    let {params: { id }} = req;
+    let { body: newUpdater } = req;
+    if (user.length == 0) {
+      res.status(404).send(`Usuario con id ${req.params.id} no existe`);
+    } else {
+      const result = Service.validatos(req.body);
+      if (result.error) {
+        res.status(400).send(result.error.details[0].message);
+      } else {
+        res.send(Service.updateUser(id, newUpdater));
+      }
+    }
+  });
+
+app.delete("/:id", (req, res) => {
+    let id = req.params.id;
+    Service.deleteUser(id);
+    res.json({
+        message: `Usuario ${id} eliminado`,
+        body: {}
+    });           
+})
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
